2f67c46883f717ff076cd7335abd30e03d3df9e2
AbilSeno <48641526+AbilSeno@users.noreply.github.com>
2020-09-08 11:12:05 +0700

    Create loding.py

---
+++ etc/loding.py
@@ -0,0 +1,8 @@
+import os, sys, threading
+
+from warn.warn import *
+
+def loding():
+
+    print (f"{kin}•{kon}•{kin}•{kon}•{kin}•{kon}•{kin}•{kon}•{kin}•{kon}{kin}•{kon}•{kon}•{kin}•{kon}•{kin}•{kon}•{kin}•{kon}•{kin}•{kon}•{kin}•{kon}•{kin}•{kon}•{kin}•{kon}•{kin}•{kon}•{kin}•{kon}•{kin}•{kon}•{kin}•{kon}•{kin}•{kon}•{kin}•{kon}•{kin}•{kon}•{kin}•{kon}•{kin}")
+
